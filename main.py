732130c831d037f5c82deb0a8e17ede6d3fdd5df
mrfrig <franklin@neosmail.com>
2025-05-29 06:04:36 -0400

    2-3 add fps limit

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        delta = clock.tick(60)
+        dt = delta / 1000
 
 if __name__ == "__main__":
     main()
